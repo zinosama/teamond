1c5eaa414b4642831a4858ad841636cbd0421bf6
Zino <rhu5@u.rochester.edu>
2016-03-03 09:19:29 -0500

    updated active menu tab regex, added user nav bar on leave view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
 	def nav_link(link_text, link_path)
 		class_name = "item " + (current_page?(link_path) ? "active" : "")
 		#if sub-menu under account
-		class_name = "item active" if link_text == "Account" && request.env['PATH_INFO'].match(/\/users\/(\d)*\/edit/)
+		class_name = "item active" if link_text == "Account" && request.env['PATH_INFO'].match(/\/users\/(\d)*\/(leave)|(edit)/)
 		
 		link_to link_text, link_path, class: class_name
 	end
