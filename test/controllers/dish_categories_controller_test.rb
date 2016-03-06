0d4a3331642db07c2593a512679e8accda43e249
Zino <rhu5@u.rochester.edu>
2016-03-06 16:25:41 -0500

    added dish_category controller tests

--- test/controllers/dish_categories_controller_test.rb
+++ test/controllers/dish_categories_controller_test.rb
@@ -2,4 +2,64 @@ require 'test_helper'
 
 class DishCategoriesControllerTest < ActionController::TestCase
 	
+	def setup
+		@user = users(:ed)
+		@dish_cate = dish_categories(:cate1)
+	end	
+
+	test 'should redirect create when not logged in' do
+		assert_no_difference 'DishCategory.count' do
+			post :create, dish_category: { name: "new-cate", description: "new category", image: File.open(File.join(Rails.root, '/test/fixtures/images/salad.jpg')) }
+		end	
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect create when logged in as non-admin' do
+		log_in_as @user
+		assert_no_difference 'DishCategory.count' do
+			post :create, dish_category: { name: "new-cate", description: "new category", image: File.open(File.join(Rails.root, '/test/fixtures/images/salad.jpg')) }
+		end	
+		assert_redirected_to root_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect edit when not logged in' do
+		get :edit
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect edit when logged in as non-admin' do
+		log_in_as @user
+		get :edit
+		assert_redirected_to root_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect update when not logged in' do
+		patch :update, id: @dish_cate, dish_category: { name: "edited cate", description: "edited category", image: File.open(File.join(Rails.root, '/test/fixtures/images/salad2.jpg')) }
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect update when logged in as non-admin' do
+		log_in_as @user
+		patch :update, id: @dish_cate, dish_category: { name: "edited cate", description: "edited category", image: File.open(File.join(Rails.root, '/test/fixtures/images/salad2.jpg')) }
+		assert_redirected_to root_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect delete when not logged in' do
+		delete :destroy, id: @dish_cate
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect delete when logged in as non-admin' do
+		delete :destroy, id: @dish_cate
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
 end
+	
