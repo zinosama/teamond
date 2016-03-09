fda1fa3098e285ef0764ae58666beb324395bc77
Zino <rhu5@u.rochester.edu>
2016-03-09 17:07:25 -0500

    dish category edit integration test

--- test/integration/dish_category_create_test.rb
+++ test/integration/dish_category_create_test.rb
@@ -10,10 +10,10 @@ class DishCategoryCreateTest < ActionDispatch::IntegrationTest
 		log_in_as @user
 		get manage_recipes_url
 		assert_no_difference 'DishCategory.count' do
-			post dish_categories_url, dish_category: { name: "", description: "", image: fixture_file_upload('test/fixtures/images/too-large.jpg','images/jpeg') }
+			post dish_categories_url, dish_category: { name: "", description: "a" * 256, image: fixture_file_upload('test/fixtures/images/too-large.jpg','images/jpeg') }
 		end
 		assert_template 'shared/manage'
-		assert_select 'li', count: 2
+		assert_select 'li', count: 3
 		assert_select 'div.ui.error.message', count: 1
 	end
 
@@ -21,7 +21,7 @@ class DishCategoryCreateTest < ActionDispatch::IntegrationTest
 		log_in_as @user
 		get manage_recipes_url
 		assert_difference 'DishCategory.count', 1 do
-			post dish_categories_url, dish_category: { name: "valid_name", description: "optional", image: fixture_file_upload('test/fixtures/images/salad.jpg','images/jpeg') }
+			post dish_categories_url, dish_category: { name: "valid_name", image: fixture_file_upload('test/fixtures/images/salad.jpg','images/jpeg') }
 		end
 		assert_redirected_to manage_recipes_url
 		follow_redirect!
--- test/integration/dish_category_edit_test.rb
+++ test/integration/dish_category_edit_test.rb
@@ -4,9 +4,27 @@ class DishCategoryEditTest < ActionDispatch::IntegrationTest
 
 	def setup
 		@cate = dish_categories(:cate1)
+		@user = users(:zino)
 	end
 
 	test 'unsuccessful edit' do 
-		
+		log_in_as @user
+		get edit_dish_category_url(@cate)
+		assert_template 'dish_categories/edit'
+		patch dish_category_url(@cate), dish_category: { name: "", description: "a" * 256, image: fixture_file_upload('test/fixtures/images/too-large.jpg','images/jpeg') }
+		assert_template 'dish_categories/edit'
+		assert_select 'li', count: 3
+		assert_select 'div.ui.error.message', count: 1		
+	end
+
+	test 'successful edit' do
+		log_in_as @user
+		get edit_dish_category_url(@cate)
+		assert_template 'dish_categories/edit' 
+		patch dish_category_url(@cate), dish_category: { name: "new-name", description: "valid description", image: fixture_file_upload('test/fixtures/images/salad.jpg', 'images/jpeg') }
+		assert_redirected_to manage_recipes_url
+		follow_redirect!
+		assert_not flash.empty?
+		assert_select 'a', text: 'new-name', count: 1
 	end
 end
