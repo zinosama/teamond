e5c4fba07f9cc616c86e5e933b762e8dbbea8384
Zino <rhu5@u.rochester.edu>
2016-03-09 18:33:06 -0500

    added integration test for recipe show page

---
+++ test/integration/recipe_show_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class RecipeShowTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@recipe = recipes(:dish1)
+	end
+
+	test 'recipe show page' do
+		get recipe_path(@recipe)
+		assert_template 'recipes/show'
+		assert_select 'p', text: @recipe.name, count: 1
+		assert_select 'p', text: "$ #{@recipe.price}", count: 1
+	end
+end
