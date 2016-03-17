3822f3d112d4e4ae64f1fae309f86af904af199a
Zino <rhu5@u.rochester.edu>
2016-03-17 11:57:55 -0400

    add orderable controller tests

---
+++ app/controllers/orderables_controller.rb
@@ -0,0 +1,15 @@
+class OrderablesController < ApplicationController
+
+	def index
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+end
---
+++ test/controllers/orderables_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class OrderablesControllerTest < ActionController::TestCase
+
+	def setup
+		@orderable1 = orderables(:orderable1)
+	end
+
+	test 'should redirect index when not logged in' do
+		get :index
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end	
+
+	test 'should redirect update when not logged in' do
+		patch :update, id: @orderable1, orderable: { quantity: 2 }
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect create when not logged in' do
+		user = users(:zino)
+		dish = dishes(:dish1)
+		post :create, orderable: { buyable: dish, ownable: user, quantity: 1, unit_price: 10 }
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect destroy when not logged in' do
+		delete :destroy, id: @orderable1
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+end
