d20f0ed568d4b5c7b1661e1e3f2ea292299c2964
Zino <rhu5@u.rochester.edu>
2016-03-17 11:20:24 -0400

    add oderable model tests

--- test/models/orderable_test.rb
+++ test/models/orderable_test.rb
@@ -3,6 +3,54 @@ require 'test_helper'
 class OrderableTest < ActiveSupport::TestCase
 	
 	def setup
+		@user = users(:zino)
+		@dish = recipes(:dish1)
+		@order = orders(:order1)
+	end
 
+	test 'belongs to user' do
+		orderable = @user.orderables.build(unit_price: 10, quantity: 1)
+		assert_equal @user, orderable.ownable
+	end
+
+	test 'belongs to order' do
+		orderable = @order.orderables.build(unit_price:10, quantity: 1)
+		assert_equal @order, orderable.ownable
+	end
+
+	test 'belongs to dish' do
+		orderable = @user.orderables.build(unit_price:10, quantity: 1)
+		orderable.buyable = @dish
+		assert_equal @dish, orderable.buyable
+	end
+
+	test 'should be valid' do
+		orderable = Orderable.new(buyable: @dish, ownable: @user, unit_price: 10, quantity: 20)
+		assert orderable.valid?
+	end
+
+	test 'quantity cannot be large than 20' do
+		orderable = Orderable.new(buyable: @dish, ownable: @user, unit_price: 10, quantity: 21)
+		assert_not orderable.valid?
+	end
+
+	test 'quantity should be present' do 
+		orderable = Orderable.new(buyable: @dish, ownable: @user, unit_price: 10)
+		assert_not orderable.valid?
+	end
+
+	test 'unit_price should be present' do
+		orderable = Orderable.new(buyable: @dish, ownable: @user, quantity: 20)
+		assert_not orderable.valid?
+	end
+
+	test 'buyable should be present' do
+		orderable = Orderable.new(ownable: @user, unit_price: 10, quantity: 20)
+		assert_not orderable.valid?
+	end
+
+	test 'ownable should be present' do
+		orderable = Orderable.new(buyable: @dish, unit_price: 10, quantity: 20)
+		assert_not orderable.valid?
 	end
 end
