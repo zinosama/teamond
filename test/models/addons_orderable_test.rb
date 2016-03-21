159cd03f10781f34efdd5efdab32e1d5cbc735f1
Zino <rhu5@u.rochester.edu>
2016-03-21 18:00:53 -0400

    add fixtures for milktea components; add model test for addon_orderable

--- test/models/addons_orderable_test.rb
+++ test/models/addons_orderable_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class AddonsOrderableTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		addon = milktea_addons(:milktea_addon1)
+		milktea_orderable = milktea_orderables(:milktea_orderable1)
+		@record = AddonsOrderable.new(milktea_addon: addon, milktea_orderable: milktea_orderable)
+	end
+
+	test 'should be valid' do
+		assert @record.valid?
+	end
+
+	test 'should have milktea addon' do
+		@record.milktea_addon = nil
+		assert_not @record.valid?
+	end
+
+	test 'should have milktea orderable' do
+		@record.milktea_orderable = nil
+		assert_not @record.valid?
+	end
 end
