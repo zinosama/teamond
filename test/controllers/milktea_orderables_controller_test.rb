00c23c1a6f099f80f7bf06e04079dc919c83b3da
Zino <rhu5@u.rochester.edu>
2016-03-21 17:45:57 -0400

    add controller test for new action in milktea orderable

--- test/controllers/milktea_orderables_controller_test.rb
+++ test/controllers/milktea_orderables_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class MilkteaOrderablesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	def setup
+		@milktea = recipes(:milktea1)
+	end
+
+	test 'should get new' do
+		get :new, milktea_id: @milktea
+		assert_response :success
+		assert_select "title", full_title("Customize Milktea")
+	end
+
 end
