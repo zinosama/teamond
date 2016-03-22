a52e2541b29cbbf16a46579d4823bd38a9101522
Zino <rhu5@u.rochester.edu>
2016-03-22 11:20:03 -0400

    add create action in milktea orderable controller; fix minor error in milktea orderable new view; add model functions for milktea orderable

--- app/controllers/milktea_orderables_controller.rb
+++ app/controllers/milktea_orderables_controller.rb
@@ -1,6 +1,6 @@
 class MilkteaOrderablesController < ApplicationController
 	before_action :logged_in_user
-	
+
 	def new
 		@milktea = Milktea.find_by(id: params[:milktea_id])
 		unless @milktea
@@ -11,9 +11,25 @@ class MilkteaOrderablesController < ApplicationController
 	end
 
 	def create
-		@milktea = Milktea.find(1)
-		@milktea_orderable = MilkteaOrderable.new
-		render "new", id: 1
+		@milktea_orderable = MilkteaOrderable.new(milktea_orderable_params)
+		if @milktea_orderable.save
+			orderable = Orderable.new(ownable: current_user, buyable: @milktea_orderable, unit_price: @milktea_orderable.unit_price, quantity: 1)
+			redirect_to menu_url
+			if orderable.save	
+				flash[:success] = "Milktea Added to Cart"
+			else
+				flash[:error] = "Error! Please login again. Contact customer service if error persists!"
+			end
+		else
+			@milktea = Milktea.find_by(id: params[:milktea_orderable][:milktea_id])
+			render "new", id: @milktea
+		end
+	end
+
+	private
+
+	def milktea_orderable_params
+		params.require(:milktea_orderable).permit(:sweet_scale, :temp_scale, :size, :milktea_id, milktea_addon_ids: [])
 	end
 
 end
--- app/models/milktea_orderable.rb
+++ app/models/milktea_orderable.rb
@@ -2,7 +2,7 @@ class MilkteaOrderable < ActiveRecord::Base
 	belongs_to :milktea
 	has_one :orderable, as: :buyable
 	
-	has_many :addons_orderables
+	has_many :addons_orderables, dependent: :destroy
 	has_many :milktea_addons, through: :addons_orderables
 
 	validates :milktea, presence: true
@@ -10,9 +10,15 @@ class MilkteaOrderable < ActiveRecord::Base
 	validates :temp_scale, presence: true, numericality: { less_than: 4, greater_than_or_equal_to: 0 }
 	validates :size, presence: true, numericality: { less_than: 2, greater_than_or_equal_to: 0 }
 
-	def addons
+	before_save :trim_addons
+
+	def unit_price
+		milktea.price + 0.99 * size.to_i + 0.5 * milktea_addons.size
 	end
 
-	def addons=
+	private
+
+	def trim_addons
+		self.milktea_addon_ids = milktea_addon_ids.select{ |id| !id.blank? }
 	end
 end
