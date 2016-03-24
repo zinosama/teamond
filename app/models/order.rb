77d7cdc7d9954b8a07e9a4bb7e2240bfb3277fb5
Zino <rhu5@u.rochester.edu>
2016-03-24 13:57:50 -0400

    add order model validations; modify order model test

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
 	belongs_to :user
 	has_many :orderables, as: :ownable
+
+	validates :total, presence: true, numericality: { greater_than: 0 }
+	validates :payment_method, presence: true, numericality: { less_than_or_equal_to: 1, greater_than_or_equal_to: 0 }
+	validates :user, presence: true
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,7 +4,7 @@ class OrderTest < ActiveSupport::TestCase
 
 	def setup
 		@user = users(:zino)
-		@order = Order.new( total: 21, payment_method: 1, paid: false, user: @user )
+		@order = Order.new( total: 2.12, payment_method: 1, paid: false, user: @user )
 	end
 
 	test 'should be valid' do
@@ -16,6 +16,11 @@ class OrderTest < ActiveSupport::TestCase
 		assert_not @order.valid?
 	end
 
+	test 'total should be number' do
+		@order.total = "Hi"
+		assert_not @order.valid?
+	end
+
 	test 'should have payment_method' do
 		@order.payment_method = ""
 		assert_not @order.valid?
@@ -24,15 +29,15 @@ class OrderTest < ActiveSupport::TestCase
 	test 'payment_method should not be larger than 1' do
 		@order.payment_method = 2
 		assert_not @order.valid?
-	end	
-
-	test 'shoud have paid' do
-		@order.paid = ""
-		assert_not @order.valid?
 	end
 
+	test 'payment_method should not be negative' do
+		@order.payment_method = -1
+		assert_not @order.valid?
+	end	
+
 	test 'should have user' do
-		@order.user = ""
+		@order.user = nil
 		assert_not @order.valid?
 	end
 end
