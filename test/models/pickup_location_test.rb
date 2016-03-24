2a21adf9f863a423f75e38609c500d6087961ffc
Zino <rhu5@u.rochester.edu>
2016-03-24 16:07:49 -0400

    add model tests for pickup location

--- test/models/pickup_location_test.rb
+++ test/models/pickup_location_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class PickupLocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@location = PickupLocation.new( name: "Wilson Commons", address: "500 Joseph c wilson blvd", description: "front door facing gym")
+	end
+
+	test 'should be valid' do
+		assert @location.valid?
+	end	
+
+	test 'should have name' do
+		@location.name = ""
+		assert_not @location.valid?
+	end
+
+	test 'name should not be too long' do
+		@location.name = "a" * 31
+		assert_not @location.valid?
+	end
+
+	test 'should have address' do
+		@location.address = ""
+		assert_not @location.valid?
+	end
+
+	test 'address should not be too long' do
+		@location.address = "a" * 256
+		assert_not @location.valid?
+	end
+
+	test 'description should not be too long' do 
+		@location.description = "a" * 256
+		assert_not @location.valid?
+	end
 end
