aea31d300e5fda0eb47f7dce33e886c9dc5b9528
Zino <rhu5@u.rochester.edu>
2016-03-24 16:22:22 -0400

    add new route, new action, and new view for pickup location

--- app/controllers/pickup_locations_controller.rb
+++ app/controllers/pickup_locations_controller.rb
@@ -1,3 +1,8 @@
 class PickupLocationsController < ApplicationController
-	
+	before_action :logged_in_user
+	before_action :logged_in_admin
+
+	def new
+		@location = PickupLocation.new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
 
   get 'summary' => 'orders#new'
   resources :orders, only: [:create]
+
+  resources :pickup_locations, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
