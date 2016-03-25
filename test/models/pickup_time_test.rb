11f6e696f9423ea67df5351a010a2bd1502763ee
Zino <rhu5@u.rochester.edu>
2016-03-24 21:51:56 -0400

    add model test for pickup time

--- test/models/pickup_time_test.rb
+++ test/models/pickup_time_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PickupTimeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@time = PickupTime.new( time: DateTime.now )
+	end
+
+	test 'should be valid' do
+		assert @time.valid?
+	end 
+
+	test 'should have a time' do
+		@time.time = nil
+		assert_not @time.valid?
+	end
 end
