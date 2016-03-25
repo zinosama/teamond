ab2f09f82faa39732f472913d7a01994da6234cc
Zino <rhu5@u.rochester.edu>
2016-03-25 15:49:54 -0400

    add association among location_time, pickup_location, and pickup time; add validations to locations_time

--- app/models/locations_time.rb
+++ app/models/locations_time.rb
@@ -1,2 +1,8 @@
 class LocationsTime < ActiveRecord::Base
+	belongs_to :pickup_time
+	belongs_to :pickup_location
+
+	validates :day_of_week, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 6 }
+	validates :pickup_time, presence: true
+	validates :pickup_location, presence: true
 end
--- app/models/pickup_time.rb
+++ app/models/pickup_time.rb
@@ -1,4 +1,7 @@
 class PickupTime < ActiveRecord::Base
+	has_many :locations_times
+	has_many :pickup_locations, through: :locations_times
+
 	validates :pickup_hour, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
 	validates :pickup_minute, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }
 	validates :cutoff_hour, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
