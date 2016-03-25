030cf24fe4c5cf42ee15a0ef7a2663c95d5060e0
Zino <rhu5@u.rochester.edu>
2016-03-25 14:50:09 -0400

    add model validations for pickup time

--- app/models/pickup_time.rb
+++ app/models/pickup_time.rb
@@ -1,3 +1,7 @@
 class PickupTime < ActiveRecord::Base
-	validates :time, presence: true	
+	validates :pickup_hour, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
+	validates :pickup_minute, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }
+	validates :cutoff_hour, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
+	validates :cutoff_minute, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 59}
+
 end
