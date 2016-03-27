230e72fa6f39f8818e6ee6a013885a4bdcb07357
Zino <rhu5@u.rochester.edu>
2016-03-27 14:04:34 -0400

    add controller test destroy for locations_time

--- test/controllers/locations_times_controller_test.rb
+++ test/controllers/locations_times_controller_test.rb
@@ -6,6 +6,7 @@ class LocationsTimesControllerTest < ActionController::TestCase
 		@user = users(:ed)
 		@location = pickup_locations(:one)
 		@time = pickup_times(:one)
+		@locations_time = LocationsTime.create( pickup_location: @location, pickup_time: @time, day_of_week: 1)
 	end
 
 	test 'should redirect create when not logged in'  do
@@ -17,6 +18,19 @@ class LocationsTimesControllerTest < ActionController::TestCase
 	test 'should redirect create when not logged in as admin' do
 		log_in_as @user
 		post :create, pickup_location_id: @location, locations_time: { pickup_time: [@time], day_of_week: [1] }
+		assert_redirected_to root_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect destroy when not logged in' do 
+		delete :destroy, @locations_time
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect destroy when not logged in as admin' do
+		log_in_as @user
+		delete :destroy, @locations_time
 		assert_redirected_to root_url
 		assert_not flash.empty?
 	end
