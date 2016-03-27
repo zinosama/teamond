4a48944d4b63d2b11c3651fd1e161e2c8676d42b
Zino <rhu5@u.rochester.edu>
2016-03-27 07:42:52 -0400

    add controller test for location_times create action

--- test/controllers/locations_times_controller_test.rb
+++ test/controllers/locations_times_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class LocationsTimesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		# @record = LocationsTime.new( pickup_location: pickup_locations(:one), pickup_time: pickup_times(:one), day_of_week: 1)
+		@user = users(:ed)
+	end
+
+	test 'should redirect create when not logged in'  do
+		post :create, locations_time: { pickup_location: pickup_locations(:one), pickup_time: pickup_times(:one), day_of_week: 1 }
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect create when not logged in as admin' do
+		log_in_as @user
+		post :create, locations_time: { pickup_location: pickup_locations(:one), pickup_time: pickup_times(:one), day_of_week: 1 }
+		assert_redirected_to root_url
+		assert_not flash.empty?
+	end
+
 end
