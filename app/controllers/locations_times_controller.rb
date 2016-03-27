1163e155e7fa9c045eddce5616735a6648182322
Zino <rhu5@u.rochester.edu>
2016-03-27 07:45:07 -0400

    add route and creat action for location_times; add controller access controller for location_times

--- app/controllers/locations_times_controller.rb
+++ app/controllers/locations_times_controller.rb
@@ -1,2 +1,8 @@
 class LocationsTimesController < ApplicationController
+	before_action :logged_in_user
+	before_action :logged_in_admin
+	
+	def create
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
 
   resources :pickup_locations, only: [:index, :create, :show]
   resources :pickup_times, only: [:index, :create]
+  resources :locations_times, only: [:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
