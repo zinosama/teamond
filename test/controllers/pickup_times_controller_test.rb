f68340cc5e672b2cfb4639e2417c6f9bc1ab005f
Zino <rhu5@u.rochester.edu>
2016-03-28 08:22:11 -0400

    add controller tests for pickup time edit and update

--- test/controllers/pickup_times_controller_test.rb
+++ test/controllers/pickup_times_controller_test.rb
@@ -4,6 +4,7 @@ class PickupTimesControllerTest < ActionController::TestCase
 
 	def setup 
 		@user = users(:ed)
+		@time = pickup_times(:one)
 	end
 
 	test 'should redirect create if not logged in' do
@@ -18,5 +19,31 @@ class PickupTimesControllerTest < ActionController::TestCase
 		assert_redirected_to root_url
 		assert_not flash.empty?
 	end
+
+	test 'should redirect edit if not logged in' do
+		get :edit, id: @time
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should redirect edit if not logged in as admin' do
+		log_in_as @user
+		get :edit, id: @time
+		assert_redirected_to root_url
+		assert_not flash.empty?
+	end
 	
+	test 'should update if not logged in' do
+		patch :update, id: @time
+		assert_redirected_to login_url
+		assert_not flash.empty?
+	end
+
+	test 'should update if not logged in as admin' do
+		log_in_as @user
+		patch :update, id: @time
+		assert_redirected_to root_url
+		assert_not flash.empty?
+	end
+
 end
