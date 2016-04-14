b176d4c682433be968b3466fcd0e29af2900c073
Zino <rhu5@u.rochester.edu>
2016-04-14 14:42:32 -0400

    remove dish_category destroy tests; modify create integration test to check initail active state of dish category

--- test/integration/dish_category_create_test.rb
+++ test/integration/dish_category_create_test.rb
@@ -22,18 +22,20 @@ class DishCategoryCreateTest < ActionDispatch::IntegrationTest
 		get manage_recipes_url
 		
 		#this category should always be there
-		assert_select 'a.ui.tag.label', text: "Taichi Milktea", count: 1
+		assert_select 'a.orange.ui.tag.label', text: "Taichi Milktea", count: 1
 		assert_select 'a.ui.tag.label', text: "valid_name", count: 0
 
 		assert_difference 'DishCategory.count', 1 do
 			post dish_categories_url, dish_category: { name: "valid_name" }
 		end
 		dish_category = assigns(:dish_category)
-		
+		assert_equal false, dish_category.active
+
 		assert_redirected_to manage_recipes_url
 		follow_redirect!
 		assert_not flash[:success].empty?
-		assert_select 'a.ui.tag.label[href=?]', edit_dish_category_url(dish_category), text: "valid_name", count: 1
+
+		assert_select 'a.ui.red.tag.label[href=?]', edit_dish_category_url(dish_category), text: "valid_name", count: 1
 	end
 
 end
