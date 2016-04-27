5bf04d312f3e0ec30c49e7a63e2c22836ac6fedd
Zino <rhu5@u.rochester.edu>
2016-04-27 16:31:36 -0400

    add association between provider and store

--- app/models/roles/provider.rb
+++ app/models/roles/provider.rb
@@ -1,5 +1,6 @@
 class Provider < ActiveRecord::Base
 	has_one :user, as: :role
-	
+	belongs_to :store
+
 	validates :user, presence: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,4 @@
 class Store < ActiveRecord::Base
+	has_many :providers
+	
 end
