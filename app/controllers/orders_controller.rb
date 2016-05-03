79c443e6128802099c10bfa68a0b830d3f80f534
Zino <rhu5@u.rochester.edu>
2016-05-02 20:18:46 -0400

    refactor update action of order controller; add order policy

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,13 +5,12 @@ class OrdersController < ApplicationController #this controller uses current_use
 	before_action :valid_cart, only: [:new, :create]
 	before_action :order_generation_router, only: [:new, :create]
 
+	before_action :valid_order, only: [:show, :update]
 
-	# before_action :correct_user_or_admin_order, only: [:show, :update]
 	# before_action :correct_user_index, only: [:index]
 
 	def index
-		@user = User.find(params[:user_id])
-		if @user.admin?
+		if current_user.admin?
 			query_hash = {
 				"unpaid" => {payment_status: 0},
 				"unfulfilled" => {fulfillment_status: 0},
@@ -28,7 +27,7 @@ class OrdersController < ApplicationController #this controller uses current_use
 			@orders = Order.where(query_hash[params[:query]]).order(created_at: :desc)
 			render('orders/index/admin') 
 		else
-			@orders = @user.orders.order(created_at: :desc)
+			@orders = current_user.role.orders.order(created_at: :desc)
 			render('orders/index/user')
 		end
 	end
@@ -65,42 +64,25 @@ class OrdersController < ApplicationController #this controller uses current_use
 	end
 
 	def show 
-		@order = Order.find_by(id: params[:id])
-		redirect_and_flash( user_orders_url(current_user), :error, "Unidentified order" ) unless @order
+		authorize @order
 	end 
 
 	def update
-		@order = Order.find_by(id: params[:id])
-		
-		if @order && params[:admin_form] && current_user.admin?
-			@order.update_attributes( order_params_update_admin )
-			@order.update_attribute( :issue_status, 2 ) if params[:order][:issue_status] == "2" && @order.issue_status == 1
-			@order.update_attribute( :issue_status, 1 ) if params[:order][:issue_status] == "100" && @order.issue_status == 2
-			redirect_and_flash(user_orders_url(current_user, query: params[:query]), :success, "Order updated")
-
-		elsif @order && params[:admin_form].nil? && !current_user.admin?
-			
-			if @order.issue_status == 0
-				if @order.update_attributes( order_params_update_user )
-					@order.update_attribute(:issue_status, 1) if @order.issue && !@order.issue.empty?
-					redirect_and_flash(order_url(@order), :success, "Thank you for your feedback")
-				else
-					flash.now[:error] = "Error. Please limit your feedback to under 255 characters."
-					render 'show'
-				end
-			elsif @order.issue_status == 3
-				@order.update_attribute(:satisfaction, params[:order][:satisfaction])
-				redirect_and_flash(order_url(@order), :success, "Thank you for your feedback")
-			else
-				@order.update_attribute( :issue_status, 3) if !params[:solved].nil? && params[:solved] == "1"
-				redirect_and_flash(order_url(@order), :success, "Thank you for your update")
-			end
-
-		else
-			redirect_and_flash(user_orders_url(current_user), :error, "Error. Unidentified order or unauthorized access" )
-		end
+		authorize @order		
+		@order.assign_attributes( order_update_params )
+		@order.issue = params[:order][:issue] if @order.no_issue?
+		@order.issue_status = 2 if params[:solved] == "1"
+		raise Exceptions::InvalidOrderAttrsError unless @order.valid?
+	rescue Exceptions::InvalidOrderAttrsError
+		flash.now[:error] = "Error! Please limit your input to under 255 characters"
+		render 'show'
+	else
+		@order.save
+		admin_route = admin_orders_url(current_user.role, query: params[:query])
+		shopper_route = order_url(@order)
+		msg = "Updated successfully"
+		redirect_and_flash( (current_user.admin? ? admin_route : shopper_route), :success, msg)
 	end
-
 
 	private
 		
@@ -136,34 +118,19 @@ class OrdersController < ApplicationController #this controller uses current_use
 				redirect_and_flash(shopper_cart_url(@shopper), :error, "Please remove unavailable or verify changed items before checkout")
 			end
 		end
-		# def order_params_update_user
-		# 	params.require(:order).permit(:satisfaction, :issue)
-		# end
 
-		# def order_params_update_admin
-		# 	params.require(:order).permit(:fulfillment_status, :solution, :note, :payment_status)
-		# end
+ 		def valid_order
+			@order = Order.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			redirect_and_flash(shopper_orders_url(current_user.role), :error, "Invalid request")
+ 		end
+
+ 		def order_update_params
+ 			params.require(:order).permit(policy(@order).permitted_update_attributes)
+ 		end
 
 		# def order_params_create
 		# 	params.require(:order).permit(:payment_method, :recipient_name, :recipient_phone, :recipient_wechat)
 		# end
-
-		def correct_user_index
-			user = User.find_by(id: params[:user_id])
-			if user
-				redirect_and_flash(root_url, :error, "Unauthorized request") unless user == current_user
-			else
-				redirect_and_flash(root_url, :error, "Unidentified user")
-			end
-		end
-
-		def correct_user_or_admin_order
-			order = Order.find_by(id: params[:id])
-			if order
-				redirect_and_flash(root_url, :error, "Unauthorized request") unless ((order.user == current_user) || current_user.admin?)
-			else
-				redirect_and_flash(root_url, :error, "Unidentified order")
-			end
-		end
 
 end
