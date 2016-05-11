85f5c5854d1963a0ac00949379dd5d9f52bfcc86
Zino <rhu5@u.rochester.edu>
2016-05-10 20:16:53 -0400

    fix order index integration test

--- test/integration/order_index_test.rb
+++ test/integration/order_index_test.rb
@@ -3,22 +3,22 @@ require 'test_helper'
 class OrderIndexTest < ActionDispatch::IntegrationTest
 
 	def setup
-		@user = users(:ed)
+		@shopper = users(:ed)
 		@order = orders(:two)
 		@admin = users(:zino)
 	end
 
-	test 'user index should list correct items' do
-		log_in_as @user
-		get user_orders_url(@user)
-		assert_select 'div.ui.fluid.card', count: 1
+	test 'shopper index should list correct items' do
+		log_in_as @shopper
+		get shopper_orders_url(@shopper.role)
+		assert_select 'div.ui.fluid.card', count: 2
 		assert_select 'form[action=?]', order_path(@order), count: 1
 		assert_select 'a[href=?]', order_url(@order), count: 1
 	end
 
 	test 'admin index should list correct items' do
 		log_in_as @admin
-		get user_orders_url(@admin)
+		get admin_orders_url(@admin.role)
 		assert_select 'div.adminOrderCard', count: 2
 		assert_select 'a[href=?]', order_url(@order), count: 1
 		assert_select 'form[action=?]', order_path(@order), count: 1
@@ -29,7 +29,7 @@ class OrderIndexTest < ActionDispatch::IntegrationTest
 		create_order(:paid)
 		assert_equal 3, Order.count
 
-		get user_orders_url(@admin, query: "unpaid")
+		get admin_orders_url(@admin.role), query: "unpaid"
 		assert_select 'div.adminOrderCard', count: 2
 	end
 
@@ -38,7 +38,7 @@ class OrderIndexTest < ActionDispatch::IntegrationTest
 		create_order(:fulfilled)
 		assert_equal 3, Order.count
 
-		get user_orders_url(@admin, query: "unfulfilled")
+		get admin_orders_url(@admin.role), query: "unfulfilled"
 		assert_select 'div.adminOrderCard', count: 2
 	end
 
@@ -47,27 +47,27 @@ class OrderIndexTest < ActionDispatch::IntegrationTest
 		create_order(:fulfilled)
 		assert_equal 3, Order.count
 
-		get user_orders_url(@admin, query: "fulfilled")
+		get admin_orders_url(@admin.role), query: "fulfilled"
 		assert_select 'div.adminOrderCard', count: 1
 	end
 
 	test 'admin problemed filter is valid' do
 		log_in_as @admin
 		create_order(:pending_issue)
-		create_order(:resolved_issue)
+		create_order(:closed_issue)
 		assert_equal 4, Order.count
 		
-		get user_orders_url(@admin, query: "complained")
+		get admin_orders_url(@admin.role), query: "complained"
 		assert_select 'div.adminOrderCard', count: 1
 	end
 
 	test 'admin resolved filter is valid' do
 		log_in_as @admin
 		create_order(:pending_issue)
-		create_order(:resolved_issue)
+		create_order(:closed_issue)
 		assert_equal 4, Order.count
 
-		get user_orders_url(@admin, query: "resolved")
+		get admin_orders_url(@admin.role), query: "resolved"
 		assert_select 'div.adminOrderCard', count: 1
 	end
 
@@ -75,13 +75,13 @@ class OrderIndexTest < ActionDispatch::IntegrationTest
 
 	def create_order(type)
 		if type == :paid
-			Order.create( user: @user, total: 10, payment_method: 1, payment_status: 1, delivery_location: locations_times(:one).pickup_location.name, delivery_time: locations_times(:one).pickup_time_datetime, recipient_name: "zinosama", recipient_phone: 3248738 )
+			Order.create( shopper: @shopper.role, total: 10, payment_method: 1, payment_status: 1, delivery_location: locations_times(:one).pickup_location.name, delivery_time: locations_times(:one).pickup_time_datetime, recipient_name: "zinosama", recipient_phone: 3248738 )
 		elsif type == :fulfilled
-			Order.create( user: @user, total: 10, payment_method: 1, payment_status: 1, delivery_location: locations_times(:one).pickup_location.name, delivery_time: locations_times(:one).pickup_time_datetime, recipient_name: "zinosama", fulfillment_status: 1, recipient_phone: 3248738 )
+			Order.create( shopper: @shopper.role, total: 10, payment_method: 1, payment_status: 1, delivery_location: locations_times(:one).pickup_location.name, delivery_time: locations_times(:one).pickup_time_datetime, recipient_name: "zinosama", fulfillment_status: 1, recipient_phone: 3248738 )
 		elsif type == :pending_issue
-			Order.create( user: @user, total: 10, payment_method: 1, delivery_location: locations_times(:one).pickup_location.name, delivery_time: locations_times(:one).pickup_time_datetime, recipient_name: "zinosama", recipient_phone: 3248738, issue_status: 2, issue: "hi")
-		elsif type == :resolved_issue
-			Order.create( user: @user, total: 10, payment_method: 1, delivery_location: locations_times(:one).pickup_location.name, delivery_time: locations_times(:one).pickup_time_datetime, recipient_name: "zinosama", recipient_phone: 3248738, issue_status: 3, issue: "hi")
+			Order.create( shopper: @shopper.role, total: 10, payment_method: 1, delivery_location: locations_times(:one).pickup_location.name, delivery_time: locations_times(:one).pickup_time_datetime, recipient_name: "zinosama", recipient_phone: 3248738, issue_status: 1, issue: "hi")
+		elsif type == :closed_issue
+			Order.create( shopper: @shopper.role, total: 10, payment_method: 1, delivery_location: locations_times(:one).pickup_location.name, delivery_time: locations_times(:one).pickup_time_datetime, recipient_name: "zinosama", recipient_phone: 3248738, issue_status: 2, issue: "hi")
 		end
 	end
 end
