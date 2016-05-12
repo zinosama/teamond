e5b1a5af2019215f3cf8fd315e44e33ac3800c8c
Zino <rhu5@u.rochester.edu>
2016-05-11 21:37:55 -0400

    add order presentor module to store order presentation methods

---
+++ app/controllers/concerns/order_presentor.rb
@@ -0,0 +1,30 @@
+module OrderPresentor
+
+	def present_fulfillment_status
+		if received?
+			{ msg: "Order received", klass: :pending }
+		elsif confirmed?
+			{ msg: "Order confirmed", klass: :confirmed }
+		elsif denied?
+			{ msg: "Store denied", klass: :error }
+		elsif in_transit?
+			{ msg: "Order in transit", klass: :warning }
+		elsif arrived?
+			{ msg: "Order arrived", klass: :success }
+		elsif delivered?
+			{ msg: "Order delivered", klass: :success }
+		elsif cancelled?
+			{ msg: "Order cancelled", klass: :error }
+		end
+	end
+
+	def present_issue_status
+		if no_feedback?
+			{ msg: "This order has no feedback", klass: :success }
+		elsif feedback_created?
+			{ msg: "Feedback has been submitted", klass: :error }
+		elsif feedback_resolved?
+			{ msg: "Feedback has been answered", klass: :pending }
+		end				
+	end
+end
